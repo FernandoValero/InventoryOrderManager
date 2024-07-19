88dc7739c891cdf4bf6683e8ed1b5d07d5423a17
Fernando Valero <55264162+FernandoValero@users.noreply.github.com>
2024-07-19 14:45:52 -0300

    Create model for Supplier entity
    - Defined the schema for the "Supplier" entity using Mongoose

--- BackEnd/models/product.js
+++ BackEnd/models/product.js
@@ -2,16 +2,15 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const ProductSchema = new Schema({
- number : { type: String, required: true },
- barCode: { type: String, required: true },
- name: { type: String, required: true },
- price: { type: Number, required: true },
- quantity: { type: Number, required: true },
- description : { type: String, required: true},
- category: { type: String, required: true},
- purchaseDate: { type: Date, required: true },
- expirationDate: { type: Date, required: true },
- supplier : { type: String, required: true},
+    number : { type: String, required: true },
+    barCode: { type: String, required: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    description : { type: String, required: true},
+    category: { type: String, required: true},
+    expirationDate: { type: Date, required: true },
+    supplier : { type: String, required: true},
 })
 
 module.exports = mongoose.models.Product || mongoose.model('Product', ProductSchema);
