4bb0aa6fa4149f3e5ce9cf9f738a7c45ec689a58
Fernando Valero <55264162+FernandoValero@users.noreply.github.com>
2024-07-21 21:04:19 -0300

    Implement MongoDB Atlas Integration
    - Configured MongoDB Atlas as the database for the application
    - Added dotenv package to manage environment variables securely
    - Created .env file to store MongoDB connection string and other sensitive data
    - Updated database connection logic in backend to use MongoDB Atlas
    - Verified successful connection to MongoDB Atlas

--- BackEnd/database.js
+++ BackEnd/database.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/InventoryOrderManager';
+
+const URI = process.env.MONGODB_URI;
+
 mongoose.connect(URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err))
